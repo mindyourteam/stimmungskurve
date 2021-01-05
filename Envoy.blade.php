d5fe4959d59b86e530ac95ac7eb050c1dff8745d
Olav Schettler <olav.schettler@dw.com>
2021-01-05 23:23:08 +0100

    Show text entry

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -32,6 +32,7 @@
     cd {{ $root_dir }};
     git clone --recursive {{ $repo }} {{ $release_dir }};
     cd {{ $release_dir }};
+    git submodule update --init --remote --recursive
 
     rm -rf storage
     rm -rf bootstrap/cache
