6c122e0b53f44780fb1a41f0f9b8a99f26c6d034
Olav Schettler <olav.schettler@dw.com>
2021-01-12 11:14:14 +0100

    #1 Permalink and title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,12 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <?php $tribe = session('tribe'); ?>
+    @if ($tribe)
+        <title>{{ $tribe->name . ' | ' . config('app.name', 'Laravel') }}</title>
+    @else
+        <title>{{ config('app.name', 'Laravel') }}</title>
+    @endif
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
